7ba57a63b9dc19f7a2e0b8e488f6df7a463b9c3c
Tengu712 <genreihoutengu@gmail.com>
2025-05-11 17:09:20 +0900

    lualine.nvim追加

--- init.lua
+++ init.lua
@@ -22,6 +22,30 @@ vim.g.maplocalleader = ' '
 -- Install or load plugins
 require('lazy').setup({
   {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup({
+        sections = {
+          lualine_a = {'mode'},
+          lualine_b = {'branch'},
+          lualine_c = {'filename'},
+          lualine_x = {'encoding', 'fileformat', 'filetype'},
+          lualine_y = {},
+          lualine_z = {'location'}
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {'filename'},
+          lualine_x = {'location'},
+          lualine_y = {},
+          lualine_z = {}
+        },
+      })
+    end
+  },
+  {
     'neovim/nvim-lspconfig',
     config = function()
       vim.diagnostic.config({
