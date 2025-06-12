c67fe089116e6400ffbbaae5336811b254603ecb
Tengu712 <genreihoutengu@gmail.com>
2025-06-13 01:18:24 +0900

    JDT LSがなくても起動できるように修正

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,42 @@
+-- JDT LSをsetupする関数
+--
+-- JDTLS_PLUGINSとJDTLS_CONFIGが読み出せないとエラーになってnvimが正常に動作しなくなるので。
+local function setup_jdtls(lspconfig, capabilities)
+  local jdtls_plugins = os.getenv('JDTLS_PLUGINS')
+  local jdtls_config = os.getenv('JDTLS_CONFIG')
+  if not jdtls_plugins or not jdtls_config then
+    return
+  end
+
+  lspconfig.jdtls.setup({
+    capabilities = capabilities,
+    cmd = {
+      'java',
+      '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+      '-Dosgi.bundles.defaultStartLevel=4',
+      '-Declipse.product=org.eclipse.jdt.ls.core.product',
+      '-Xms1g',
+      '-Xmx2G',
+      '--add-modules=ALL-SYSTEM',
+      '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+      '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+      '-jar', jdtls_plugins .. '/org.eclipse.equinox.launcher_1.7.0.v20250424-1814.jar',
+      '-configuration', jdtls_config,
+      '-data', vim.fn.expand('~/.cache/jdtls/workspace'),
+    },
+    root_dir = lspconfig.util.root_pattern('.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle'),
+    filetypes = { 'java' },
+    settings = {
+      java = {
+        eclipse = { downloadSources = true },
+        configuration = { updateBuildConfiguration = 'interactive' },
+        maven = { downloadSources = true },
+        format = { enabled = true },
+      },
+    },
+  })
+end
+
 return {
   'neovim/nvim-lspconfig',
   dependencies = { 'saghen/blink.cmp' },
@@ -8,33 +47,33 @@ return {
       virtual_text = false,
       signs = {
         text = {
-          [vim.diagnostic.severity.ERROR] = "",
-          [vim.diagnostic.severity.WARN] = "",
-          [vim.diagnostic.severity.INFO] = "",
-          [vim.diagnostic.severity.HINT] = "",
+          [vim.diagnostic.severity.ERROR] = '',
+          [vim.diagnostic.severity.WARN] = '',
+          [vim.diagnostic.severity.INFO] = '',
+          [vim.diagnostic.severity.HINT] = '',
         },
         numhl = {
-          [vim.diagnostic.severity.ERROR] = "DiagnosticLineNrError",
-          [vim.diagnostic.severity.WARN] = "DiagnosticLineNrWarn",
-          [vim.diagnostic.severity.INFO] = "DiagnosticLineNrInfo",
-          [vim.diagnostic.severity.HINT] = "DiagnosticLineNrHint",
+          [vim.diagnostic.severity.ERROR] = 'DiagnosticLineNrError',
+          [vim.diagnostic.severity.WARN] = 'DiagnosticLineNrWarn',
+          [vim.diagnostic.severity.INFO] = 'DiagnosticLineNrInfo',
+          [vim.diagnostic.severity.HINT] = 'DiagnosticLineNrHint',
         },
         linehl = {},
       },
       underline = true,
       severity_sort = true,
-      float = { border = "rounded" },
+      float = { border = 'rounded' },
     })
 
     -- カラースキーム設定
-    vim.api.nvim_create_autocmd("ColorScheme", {
-      pattern = "*",
-      group = vim.api.nvim_create_augroup("MyDiagnosticHighlights", { clear = true }),
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      pattern = '*',
+      group = vim.api.nvim_create_augroup('MyDiagnosticHighlights', { clear = true }),
       callback = function()
-        vim.api.nvim_set_hl(0, "DiagnosticLineNrError", { fg = "#FF0000", bold = true })
-        vim.api.nvim_set_hl(0, "DiagnosticLineNrWarn",  { fg = "#FFA500", bold = true })
-        vim.api.nvim_set_hl(0, "DiagnosticLineNrInfo",  { fg = "#00FFFF", bold = true })
-        vim.api.nvim_set_hl(0, "DiagnosticLineNrHint",  { fg = "#00FF00", bold = true })
+        vim.api.nvim_set_hl(0, 'DiagnosticLineNrError', { fg = '#FF0000', bold = true })
+        vim.api.nvim_set_hl(0, 'DiagnosticLineNrWarn',  { fg = '#FFA500', bold = true })
+        vim.api.nvim_set_hl(0, 'DiagnosticLineNrInfo',  { fg = '#00FFFF', bold = true })
+        vim.api.nvim_set_hl(0, 'DiagnosticLineNrHint',  { fg = '#00FF00', bold = true })
       end,
     })
 
@@ -55,8 +94,8 @@ return {
         ['rust-analyzer'] = {
           checkOnSave = {
             enable = true,
-            command = "clippy",
-            extraArgs = {"--no-deps"},
+            command = 'clippy',
+            extraArgs = {'--no-deps'},
           },
           lens = { enable = false },
         },
@@ -64,32 +103,6 @@ return {
     })
 
     -- Java
-    lspconfig.jdtls.setup({
-      capabilities = capabilities,
-      cmd = {
-        'java',
-        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-        '-Dosgi.bundles.defaultStartLevel=4',
-        '-Declipse.product=org.eclipse.jdt.ls.core.product',
-        '-Xms1g',
-        '-Xmx2G',
-        '--add-modules=ALL-SYSTEM',
-        '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-        '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-        '-jar', os.getenv('JDTLS_PLUGINS') .. '/org.eclipse.equinox.launcher_1.7.0.v20250424-1814.jar',
-        '-configuration', os.getenv('JDTLS_CONFIG'),
-        '-data', vim.fn.expand('~/.cache/jdtls/workspace'),
-      },
-      root_dir = lspconfig.util.root_pattern('.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle'),
-      filetypes = { 'java' },
-      settings = {
-        java = {
-          eclipse = { downloadSources = true },
-          configuration = { updateBuildConfiguration = "interactive" },
-          maven = { downloadSources = true },
-          format = { enabled = true },
-        },
-      },
-    })
+    setup_jdtls(lspconfig, capabilities)
   end,
 }
