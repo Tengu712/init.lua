471702f10cd407c6f2bd38cd43f15c892b1194d0
Tengu712 <genreihoutengu@gmail.com>
2026-02-14 15:07:49 +0900

    ターミナルを右に開く

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -4,12 +4,12 @@ return {
     -- setup
     require('toggleterm').setup({
       open_mapping = '<c-t>',
-      direction = 'float',
+      direction = 'vertical',
+      size = vim.o.columns * 0.33,
     })
 
     -- 0-9トグル
     for i = 1, 9 do
-      vim.keymap.set('n', '<C-' .. i .. '>', ':ToggleTerm' .. i .. '<CR>', { noremap = true, silent = true, desc = i .. '番目のターミナルを開く' })
       vim.api.nvim_create_user_command('TT' .. i, function()
         vim.cmd(':ToggleTerm' .. i)
       end, {  desc = '番目のターミナルを開く' })
