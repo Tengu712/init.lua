41c010bf3ab37422a1f321bf391590f24db3ded5
Tengu712 <genreihoutengu@gmail.com>
2026-02-14 15:34:53 +0900

    Neotreeのサイズを変更

--- lua/plugins/filer.lua
+++ lua/plugins/filer.lua
@@ -1,3 +1,14 @@
+function find_neo_tree_window()
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    local ft  = vim.api.nvim_buf_get_option(buf, 'filetype')
+    if ft == 'neo-tree' then
+      return win
+    end
+  end
+  return nil
+end
+
 return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
@@ -7,13 +18,14 @@ return {
     "MunifTanjim/nui.nvim",
   },
   lazy = false,
-  opts = {
-    window = { width = '15%' },
-  },
   config = function()
     -- ユーザ定義コマンド
     vim.api.nvim_create_user_command('NT', function()
-      vim.cmd('Neotree toggle')
+      vim.cmd [[Neotree toggle]]
+      local win = find_neo_tree_window()
+      if win then
+        vim.api.nvim_win_set_width(win, math.floor(vim.o.columns * 0.2))
+      end
     end, { desc = 'Neotreeをトグルで開く' })
   end,
 }
